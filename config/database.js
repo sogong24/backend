96634a101826821edf7e12324842cdc2ae20e3f0
minbros <pristo24631@gmail.com>
2024-11-22 00:40:48 +0900

    Feat: docker 활성화

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
     logging: false, // Disable logging; default: console.log
 });
