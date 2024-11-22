446ad09d1ffedacaf0a73e567c19ce18548cdf66
minbros <pristo24631@gmail.com>
2024-11-22 11:22:57 +0900

    Fix: sequelize 에러 수정

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URI, {
     dialect: 'postgres',
     logging: false, // Disable logging; default: console.log
 });
