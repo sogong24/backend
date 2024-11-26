eb64334c3ebb50db953816e04b17242eda2db621
Seungwoo Choe <seungwoochoes@gmail.com>
2024-11-26 12:27:51 +0900

    Use password hash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
 const User = require('../models/User');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.signUp = async (req, res) => {
@@ -9,8 +9,8 @@ exports.signUp = async (req, res) => {
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) return res.status(400).json({ error: 'Email already in use' });
 
-        // const passwordHash = await bcrypt.hash(password, 10);
-        const user = await User.create({ email, password, username });
+        const passwordHash = await bcrypt.hash(password, 10);
+        const user = await User.create({ email, passwordHash, username });
         res.status(201).json({ message: 'User created', userId: user.id });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -23,8 +23,7 @@ exports.logIn = async (req, res) => {
         const user = await User.findOne({ where: { email } });
         if (!user) return res.status(400).json({ error: 'Invalid credentials' });
 
-        const match = true;
-        // const match = await bcrypt.compare(password, user.passwordHash);
+        const match = await bcrypt.compare(password, user.passwordHash);
         if (!match) return res.status(400).json({ error: 'Invalid credentials' });
 
         const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -42,8 +41,7 @@ exports.editUserInfo = async (req, res) => {
         if (!user) return res.status(404).json({ error: 'User not found' });
 
         if (password) {
-            user.passwordHash = password;
-            // user.passwordHash = await bcrypt.hash(password, 10);
+            user.passwordHash = await bcrypt.hash(password, 10);
         }
         if (username) {
             user.username = username;
